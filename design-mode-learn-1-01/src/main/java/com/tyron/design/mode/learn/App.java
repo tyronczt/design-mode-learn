69ab50c802b8d11ab2091baab4df6df30d2989b3
tyron <tyron330726@gmail.com>
2023-01-29 17:55:41 +0800

    init

---
+++ design-mode-learn-1-01/src/main/java/com/tyron/design/mode/learn/App.java
@@ -0,0 +1,10 @@
+package com.tyron.design.mode.learn;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
