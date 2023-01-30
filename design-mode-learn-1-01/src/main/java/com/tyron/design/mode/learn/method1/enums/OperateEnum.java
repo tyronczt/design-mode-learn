c7715131d295c1fef96f75962de231fbcc1cad15
tyron <tyron330726@gmail.com>
2023-01-30 16:47:27 +0800

    feat：单一职责原则-方法

--- design-mode-learn-1-01/src/main/java/com/tyron/design/mode/learn/method1/enums/OperateEnum.java
+++ design-mode-learn-1-01/src/main/java/com/tyron/design/mode/learn/method1/enums/OperateEnum.java
@@ -6,6 +6,12 @@ package com.tyron.design.mode.learn.method1.enums;
  * @create: 2023-01-30
  */
 public enum OperateEnum {
+    /**
+     * 更新用户名
+     */
     UPDATE_USERNAME,
+    /**
+     * 更新密码
+     */
     UPDATE_PASSWORD;
 }
